f379ff009ee0ecfab0f13d345390b9a409248c81
Thodoris Akritidis <thodoris.akritidis98@gmail.com>
2022-12-22 21:53:36 +0200

    remove old testing library

--- vite.config.js
+++ vite.config.js
@@ -8,10 +8,7 @@ import svgLoader from "vite-svg-loader";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), svgLoader()],
-  test: {
-    globals: true,
-    environment: "happy-dom",
-  },
+
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
